455992d82039045d6e378f3f95aa0bd162eeae65
Ericson <emontero@thefactoryhka.com>
2022-10-25 14:13:30 -0400

    Initial commit

---
+++ app/src/main/java/com/ericsonmontero/moviewtechnicaltest/presentation/home/HomeFragment.kt
@@ -0,0 +1,7 @@
+package com.ericsonmontero.moviewtechnicaltest.presentation.home
+
+import androidx.fragment.app.Fragment
+import com.ericsonmontero.moviewtechnicaltest.R
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+}
